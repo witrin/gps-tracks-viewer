48b437c76192b74c189d6ca416a113e077ec78b2
Artus Kolanowski <artus@ionoi.net>
2017-07-23 22:12:48 +0200

    Improve list element

--- src/elements/list/index.js
+++ src/elements/list/index.js
@@ -11,34 +11,60 @@ export default class ListElement extends HTMLElement {
 	constructor() {
 		// call parent constructor
 		super();
-		// all template tags
+		// template tags
 		this._templates = {};
-		// all current offsets
+		// current offsets
 		this._offsets = {
 			items: 0,
 			children: this.children.length,
-			pages: new Set([0])
+			pages: [0]
 		};
-		// all current data
+		// current length
+		this._length = 0;
+		// current item
+		this._selected = null;
+		// current page
+		this._page = 0;
+		// current data
 		this._items = [];
-		// semaphore for the rendering
-		this._rendering = false;
 	}
 
 	/**
 	 * Get the number of pages
 	 */
 	get pages() {
-		return this._offsets.pages.size;
+		return this._offsets.pages.length;
 	}
 
 	/**
 	 * Get the current page
 	 */
 	get page() {
-		return Array.from(this._offsets.pages.values()).sort(function (a, b) {
-			return a - b;
-		}).indexOf(this._offsets.items);
+		return this._offsets.pages.indexOf(this._offsets.items);
+	}
+
+	/**
+	 * Get the selected data
+	 */
+	get selected() {
+		return this._selected;
+	}
+
+	/**
+	 * Set the selected data
+	 */
+	set selected(item) {
+		// create the key
+		let key = JSON.stringify(item);
+		// find the key
+		if (this.items.find(function (data) {
+			return JSON.stringify(data) === key;
+		})) {
+			// set the selected
+			this._selected = item;
+			// update the element
+			this._update();
+		}
 	}
 
 	/**
@@ -52,12 +78,10 @@ export default class ListElement extends HTMLElement {
 	 * Set the element data
 	 */
 	set items(items) {
-		// reset offsets
-		this._offsets.items = 0;
 		// assign the data
 		this._items = Array.from(items);
 		// update the element
-		this._update();
+		this._reset();
 	}
 
 	/**
@@ -67,7 +91,7 @@ export default class ListElement extends HTMLElement {
 		this._templates.item = this.querySelector("[data-template=item]");
 
 		window.addEventListener("resize", this._onResize.bind(this), false);
-		this._update();
+		this._reset();
 	}
 
 	/**
@@ -81,134 +105,112 @@ export default class ListElement extends HTMLElement {
 	 * Scroll next page
 	 */
 	next() {
-		let next = Array.from(this._offsets.pages.values())
-			.filter((function (offset) {
-				return offset > this._offsets.items;
-			}).bind(this))
-			.reduce(function (a, b) {
-				return Math.min(a, b);
-			}, this._items.length);
-
-		if (next > this._offsets.items && next < this._items.length && next > -1) {
-			this._offsets.items = next;
-			this._render();
-		}
+		this.jump(this._page + 1);
 	}
 
 	/**
 	 * Scroll previous page
 	 */
 	previous() {
-		let previous = Array.from(this._offsets.pages.values())
-			.filter((function (offset) {
-				return offset < this._offsets.items;
-			}).bind(this))
-			.reduce(function (a, b) {
-				return Math.max(a, b);
-			}, -1);
+		this.jump(this._page - 1);
+	}
 
-		if (previous < this._offsets.items && previous < this._items.length && previous > -1) {
-			this._offsets.items = previous;
-			this._render();
+	/**
+	 * Jump to the given page
+	 *
+	 * @param {number} page The page index
+	 */
+	jump(page) {
+		console.log("Jump to", page);
+		this._page = this._limit(page, "pages");
+
+		if (this._offsets.items !== this._offsets.pages.indexOf(this._page)) {
+			this._offsets.items = this._offsets.pages[this._page];
+			this._update();
 		}
+	}
+
+	/**
+	 * Limit the given position of a section
+	 *
+	 * @param {number} position The position to limit
+	 * @param {string} section The section of the given position
+	 */
+	_limit(position, section) {
+		return Math.max(0, Math.min(position, this._offsets[section].length - 1));
 	}
 
 	/**
 	 * Event handler when the element is resized
 	 */
 	_onResize() {
-		if (this._rendering) {
-			return;
-		}
-
 		window.requestAnimationFrame((function () {
-			this._update();
+			this._reset();
 		}).bind(this));
 	}
 
 	/**
-	 * Render the element
-	 *
-	 * @param {boolean}
+	 * Update the list
 	 */
-	_render(prerender = false) {
-		// set semaphore for the rendering
-		this._rendering = true;
+	_update() {
 		// render the list from top, as long as there is still enough space for it
-		let i = -1;
-		do {
-			// increment position
-			++i;
-			// check if there is still data to render
-			if (this._offset(i, "items") >= this._items.length) {
-				break;
-			}
-			// create new element if none exist on position
-			if (this._offset(i, "children") >= this.children.length) {
+		for (let i = 0; this._offset(i, "items") < this._items.length; this._length = ++i) {
+			// create new element if list is too short
+			if (this._offset(i, "children") === this.children.length) {
 				let template = document.importNode(this._templates.item.content, true);
-				template.children[0].style.visibility = "hidden";
 				this.appendChild(template.children[0]);
 			}
-			// bind data to element at position
+			// bind data to current element
 			this._bind(
 				this._items[this._offset(i, "items")],
 				this.children[this._offset(i, "children")]
 			);
-		} while (
-			this._calculateHeight(
-				this._offset(0, "children"),
-				this._offset(i, "children")
-			) <= this.clientHeight
-		);
-		// check if the position is fully within the client area
-		i -= this._calculateHeight(
-			this._offset(0, "children"),
-			this._offset(i, "children")
-		) > this.clientHeight ? 2 : 1;
-		// update the visibility of all items
-		if (!prerender) {
-			Array.from(this.children)
-				.slice(this._offset(0, "children"), this.children.length)
-				.forEach(function (child, j) {
-					child.style.visibility = i > j - 1 ? "visible" : "hidden";
-				});
+			// check if current element is visible
+			if (!this._isVisible(this._offset(i, "children"))) {
+				break;
+			}
 		}
-		// cache start of next page
-		if (this._offset(i + 1, "items") < this._items.length) {
-			this._offsets.pages.add(this._offset(i + 1, "items"));
-		}
-		// unset the semaphore for rendering
-		this._rendering = false;
+		// update children classes
+		Array.from(this.children)
+			.slice(this._offset(0, "children"), this.children.length)
+			.forEach(function (child, i) {
+				child.classList.toggle("inactive", i >= this._length);
+			}.bind(this));
 	}
 
 	/**
-	 * Update the element
+	 * Reset the list
 	 */
-	_update() {
-		// preserve the current data offset
-		let data = this._offsets.items;
-		// reset the current data offset
+	_reset() {
+		// reset the offsets
 		this._offsets.items = 0;
-		// reset offsets for pages
-		this._offsets.pages = new Set([0]);
-		// calculate offsets for pages by rendering all pages
-		for (let i = 0; i <= this._offsets.pages.size; i++) {
-			this._render(true);
-			this._offsets.items = Array.from(this._offsets.pages.values())
-				.reduce(function (a, b) {
-					return Math.max(a, b);
+		this._offsets.pages = [0];
+		this._page = 0;
+		// pre-calculate offsets for pages by rendering all pages,
+		// this is a nasty bottleneck but gives us much flexibility
+		// regarding the CSS layout
+		for (let i = 0; i <= this._offsets.pages.length; i++) {
+			// pre-render the next page
+			this._update();
+			// update offset of next page
+			if (this._offset(this._length, "items") < this._items.length) {
+				this._offsets.pages.push(this._offset(this._length, "items"));
+				this._offsets.pages = this._offsets.pages.sort(function (a, b) {
+					return a - b;
 				});
+				this._offsets.items = this._offsets.pages.slice(-1).pop();
+			}
 		}
-		// restore the current data offset
-		this._offsets.items = data;
-		// render the current data offset
-		this._render();
+		// reset the data offset
+		this._offsets.items = 0;
+		// render the page
+		this._update();
 	}
 
 	/**
 	 * Bind data to an element
 	 *
-	 * @param {Object} data The data 1to bind
+	 * @param {Object} data The data to bind
 	 * @param {HTMLElement} element The element to bind the data to
 	 */
 	_bind(data, element) {
@@ -224,22 +226,8 @@ export default class ListElement extends HTMLElement {
 			});
 		// attach data
 		element.dataset.source = JSON.stringify(data);
-	}
-
-	/**
-	 * Calculate the height of the elements within a given range
-	 *
-	 * @param {number} start The start of the range
-	 * @param {number} end The end of the range
-	 */
-	_calculateHeight(start, end) {
-		return Array.from(this.children)
-			.slice(
-				start,
-				end
-			).reduce(function (height, item) {
-				return height + item.offsetHeight;
-			}, 0);
+		// toggle selected
+		element.classList.toggle("selected", element.dataset.source === JSON.stringify(this._selected));
 	}
 
 	/**
@@ -250,5 +238,20 @@ export default class ListElement extends HTMLElement {
 	 */
 	_offset(position, section) {
 		return this._offsets[section] + position;
+	}
+
+	/**
+	 * Wether the item is at the given position visible or not
+	 *
+	 * @param {number} position The position of a child
+	 * @returns {boolean}
+	 */
+	_isVisible(position) {
+		if (position >= this.children.length || position <= -1) {
+			return undefined;
+		}
+
+		let bounding = this.children[position].getBoundingClientRect();
+		return bounding.top >= 0 && bounding.bottom < window.innerHeight;
 	}
 }
